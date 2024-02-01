6b330c741964cf6ea85a226cedd5f44de4b32609
Mo Shihab <Mohammedshihab6969@gmail.com>
2024-02-01 00:39:04 +0000

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,63 +4,75 @@ import { FaHandPointer, FaTimes } from 'react-icons/fa';
 import { useState } from 'react';
 
 const Header = () => {
-  let [rightPosition, setRightPosition] = useState(-200);
+  const [rightPosition, setRightPosition] = useState(-200);
+  const [menuVisibility, setMenuVisibility] = useState('none');
+
+  const toggleMenu = () => {
+    setRightPosition((prevRightPosition) =>
+      prevRightPosition === 0 ? -200 : 0
+    );
+
+    setMenuVisibility((prevVisibility) =>
+      prevVisibility === 'flex' ? 'none' : 'flex'
+    );
+  };
 
   return (
-    <header className="container m-auto">
-      <div className="flex pt-4 items-center">
+    <header className=" ">
+      <div className=" container m-auto flex pt-4 items-center">
         <div className="flex-1">
-          <span className=" text-[30px] lg:text-4xl font-bold">
+          <span className="text-[30px] lg:text-4xl font-bold">
             LEARN<span className="text-[--bcGold]">EGYPT</span>
           </span>
         </div>
-        <div className=" flex-1 flex justify-end mr-4">
-          <ul className="hidden lg:flex list-none gap-4 justify-end  lg:text-[22px] lg:justify-evenly font-semibold tracking-wide ">
+        <div className="flex-1 flex justify-end mr-4">
+          <ul className="hidden lg:flex lg:flex-1 list-none gap-4 justify-end lg:mt-4 lg:text-[22px] lg:justify-evenly font-semibold tracking-wide ">
             <li className="">
-              <a href="#">About us</a>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Explore</a>
             </li>
             <li>
               <a href="#">Videos</a>
             </li>
             <li>
-              <a href="#">Contact us</a>
+              <a href="#">Blog</a>
             </li>
           </ul>
-          <div className="lg:hidden sm:flex ">
+          <div className="lg:hidden sm:flex mr-8">
             <GiHamburgerMenu
               color="white"
               size={'32px'}
-              onClick={() =>
-                setRightPosition((prevRightPosition) =>
-                  prevRightPosition === 0 ? -200 : 0
-                )
-              }
+              onClick={toggleMenu}
               cursor={'pointer'}
             />
             <ul
-              style={{ right: `${rightPosition}px`, transition: '.6s ease-in' }}
-              className=" text-[20px] flex flex-col items-end gap-10 absolute top-[0px] bg-slate-400 bg-opacity-50 h-screen z-[100] px-9 py-6"
+              style={{
+                right: `${rightPosition}px`,
+                transition: '.6s ease-in',
+                display: `${menuVisibility}`,
+              }}
+              className="text-[20px] flex-col items-end gap-10 absolute top-[0px] bg-slate-400 bg-opacity-50 h-screen z-[100] px-9 py-6"
             >
               <FaTimes
                 color="white"
                 size={'32px'}
                 className="mt-[10px]"
-                onClick={() =>
-                  setRightPosition((prevRightPosition) =>
-                    prevRightPosition === 0 ? -200 : 0
-                  )
-                }
+                onClick={toggleMenu}
                 cursor={'pointer'}
               />
               <li className="mt-[30px]">
-                {' '}
-                <a href="#">About us</a>
+                <a href="#">Home</a>
               </li>
               <li>
-                <a href="#">Contact us</a>
+                <a href="#">Explore</a>
               </li>
               <li>
                 <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
               </li>
             </ul>
           </div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import { FaInstagram } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
 import { CiYoutube } from 'react-icons/ci';
@@ -10,33 +11,54 @@ const Intro = () => {
       className="section"
       id="intro"
     >
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute h-[130%] w-[100%] object-cover top-[-200px] lg:right-0 lg:bottom-0 -z-10"
+      >
+        <source src={BgVideo} />
+      </video>
       <div className="container mx-auto">
-        <video
-          autoPlay
-          loop
-          muted
-          className="absolute h-[130%] w-dvw object-cover top-[-200px] lg:right-0 lg:bottom-0 -z-50"
-        >
-          <source src={BgVideo} />
-        </video>
-        <div className="flex flex-col lg:flex-row gap-x-60">
+        <div className="flex flex-col items-center lg:flex-row lg:gap-y-2 gap-y-20 gap-x-10">
           <div className="flex-1 flex gap-10 flex-col justify-center items-center">
-            <h1 className="text-[40px] font-bold lg:text-nowrap">
+            <h1 className="text-[42px] font-bold lg:text-nowrap">
               Ancient Egypt International Relations
             </h1>
-            <button className="text-[24px] border-[2px] border-white px-2 py-1 rounded-full">
+            <button className="text-[24px] border-[2px] px-2 py-1 rounded-full">
               Publications
             </button>
           </div>
-          <div className="flex-1">Boxes</div>
+          <div className="w-[100%] mb-4 flex items-center lg:items-center justify-center flex-col gap-5">
+            <div className="font-bold text-[30px]">
+              <TypeAnimation
+                sequence={[
+                  'Nine Bows',
+                  2000,
+                  'Maat & Enemies',
+                  2000,
+                  'Foreigners in Literature',
+                  2000,
+                  'Diplomacy and External Policies',
+                  2000,
+                  'Women in Diplomacy',
+                  2000,
+                ]}
+                speed={50}
+                className="text-[white]"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </div>
+          </div>
           <div className="flex-1 flex lg:flex-col justify-center lg:justify-end items-center ">
-            <hr className="lg:h-[200px] lg:w-[1px] w-[100px] bg-white border-[3px] dark:bg-gray-700 lg:mb-3" />
+            <hr className="lg:h-[200px] lg:w-[1px] w-[100px] bg-white border-[3px]  lg:mb-3" />
             <div className="flex lg:flex-col gap-4 mx-3">
               <FaInstagram size={'2rem'} />
               <FaXTwitter size={'2rem'} />
               <CiYoutube size={'2rem'} />
             </div>
-            <hr className="lg:h-[200px] lg:w-[1px] w-[100px] bg-white border-[3px] dark:bg-gray-700 lg:mt-3" />
+            <hr className="lg:h-[200px] lg:w-[1px] w-[100px] border-[3px]  lg:mt-3" />
           </div>
         </div>
       </div>
